c70e56ee05b7a65dca6b84b4e61ef65fbd314ea8
Ifreitasacordou <32847625+Ifreitasacordou@users.noreply.github.com>
2017-11-18 00:06:09 -0200

    Update Crawler_c01.py

--- Crawler_c01.py
+++ Crawler_c01.py
@@ -1,5 +1,17 @@
 from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
+import requests
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return 'OK!'
+
+if __name__ == "__main__":
+    app.run()
 
 def extract_titles(content):
     soup = BeautifulSoup(content, 'lxml')
